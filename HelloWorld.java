2804840ce03a55f690037f1d312f0d4eea68b3c3
Arulsundarmts <39165594+Arulsundarmts@users.noreply.github.com>
2022-08-07 19:27:18 -0400

    Update HelloWorld.java
    changes by Arul

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
 
     public static void main(String[] args){
 
-        System.out.println("Hellow World");
+        System.out.println("Hellow World by Arul");
+        
+        System.out.println("Hellow Earth");
     }
 }
